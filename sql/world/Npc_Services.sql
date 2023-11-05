319d1359d53a8af26b0ff262eaa3ee258525d1b3
Haeniken <me@haeniken.com>
2023-11-05 11:00:13 +0300

    Update Npc_Services.sql
    Localization and few changes sql

--- sql/world/Npc_Services.sql
+++ sql/world/Npc_Services.sql
@@ -1,9 +1,13 @@
-DELETE FROM `creature_template` WHERE `entry` = 55002;
-Set @NpcName = "Services NPC",
-	@NpcSubname = "AzerothCore",
-	@NpcEntry = 55002,
-	@NpcDisplayID = 31833,
+DELETE FROM `creature_template` WHERE `entry` = 190013;
+Set @NpcName = "Janus-faced",
+	@NpcSubname = "A thousand little things",
+	@NpcEntry = 190013,
+	@NpcDisplayID = 26362,
 	@NpcLevel = 80;
 
 INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `BaseAttackTime`, `RangeAttackTime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES 
 (@NpcEntry, @NpcDisplayID, @NpcName, @NpcSubname, @NpcLevel, @NpcLevel, 35, 1, 1, 1.14286, 1, 1, 0, 2000, 2000, 2, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 50, 50, 1, 0, 0, 1, 0, 0, 'Npc_Services', 12340);
+
+DELETE FROM `creature_template_locale` WHERE (`entry`=190013);
+INSERT INTO `creature_template_locale` VALUES
+(90000, 'ruRU', 'Двуликий', 'Тысяча мелочей', '13000');
